788042cab9f3e3bcf821a38750628657a23ccf02
Denise Salazar <denise.salazar.1210@gmail.com>
2018-05-18 16:32:11 -0700

    Add flyway migrations

---
+++ flyway/sql/afterMigrate.sql
@@ -0,0 +1,8 @@
+INSERT INTO USERS
+    (USER_NAME, FIRST_NAME, LAST_NAME, PASSWORD, PHONE, EMAIL)
+VALUES
+    ('atorres','Adriana','Torres','12345','123-456-7890','adriana@torres.com'),
+    ('msmit','Melinda','Smith','54321','111-111-1111','melinda@smith.com'),
+    ('kbaudelaire','Klaus','Baudelaire','vfd123','222-222-2222','klaus@baudelaire.com'),
+    ('hgranger','Hermione','Granger','leviosa42','333-333-3333','hermione@grander.com'),
+    ('jerye','Jane','Eyre','bronte1847','000-000-0000','jane@eyre.come');
