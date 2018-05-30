38eef37d8a3b9ae7dc2d2cfcba28fec7b77d98f9
Denise Salazar <denise.salazar.1210@gmail.com>
2018-05-30 14:44:43 -0700

    Remove cache files and reset ui apiHost

--- user-management/ui/src/environments/environment.prod.ts
+++ user-management/ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiHost: 'http://ec2-54-183-191-97.us-west-1.compute.amazonaws.com:8080'
+  apiHost: 'http://ec2-13-56-180-73.us-west-1.compute.amazonaws.com:8080/'
 };
