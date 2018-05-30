c6a24ab7f05e38d0b15e56c076cf3fa6e4f475dd
Denise Salazar <denise.salazar.1210@gmail.com>
2018-05-30 15:52:16 -0700

    Add list of Times Square Entertainment Venues

---
+++ user-management/ui/src/app/venues/venues.component.ts
@@ -0,0 +1,22 @@
+import { VenuesService } from './venues.service';
+import { Component, OnInit } from '@angular/core';
+import { Venue } from '../types/venue';
+
+@Component({
+  selector: 'app-venues',
+  templateUrl: './venues.component.html',
+  styleUrls: ['./venues.component.css']
+})
+export class VenuesComponent implements OnInit {
+
+  venues: Venue[];
+
+  constructor(private venuesService: VenuesService) { }
+
+  ngOnInit() {
+    this.venuesService.getScores()
+      .subscribe((response) => {
+        this.venues = response}); 
+  }
+
+}
---
+++ user-management/ui/src/app/venues/venues.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@angular/core";
+import {HttpClient} from '@angular/common/http';
+import {Venue} from './../types/venue';
+
+@Injectable()
+export class VenuesService {
+
+    venues_url = 'https://data.cityofnewyork.us/resource/2pc8-n4xe.json';
+    
+    constructor(private http: HttpClient) {
+    }
+
+    getScores() {
+        return this.http.get<Venue[]>(this.venues_url);
+    }
+}
