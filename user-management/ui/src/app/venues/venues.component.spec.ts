43db482ed0c33d2771f715db35d991848f7bf5a9
Denise Salazar <denise.salazar.1210@gmail.com>
2018-06-01 04:57:26 -0700

    Update frontend tests

--- user-management/ui/src/app/venues/venues.component.spec.ts
+++ user-management/ui/src/app/venues/venues.component.spec.ts
@@ -1,6 +1,12 @@
+import { ActivatedRoute } from '@angular/router';
+import { VenuesService } from './venues.service';
+import { VenuesMapComponent } from './../venues-map/venues-map.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VenuesComponent } from './venues.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('VenuesComponent', () => {
   let component: VenuesComponent;
@@ -8,7 +14,12 @@ describe('VenuesComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VenuesComponent ]
+      declarations: [ VenuesComponent,
+                      VenuesMapComponent ],
+      imports: [ NgxPaginationModule ,
+                 HttpClientModule,
+                 RouterTestingModule ],
+      providers: [ VenuesService ]
     })
     .compileComponents();
   }));
@@ -19,7 +30,4 @@ describe('VenuesComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
