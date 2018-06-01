476c81ccbe5ff37638c995edd907f21b2b6bfa75
Denise Salazar <denise.salazar.1210@gmail.com>
2018-06-01 12:03:12 -0700

    Change environment production apiHost

--- user-management/ui/src/environments/environment.prod.ts
+++ user-management/ui/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiHost: 'http://ec2-18-144-34-24.us-west-1.compute.amazonaws.com:8080'
+  apiHost: 'http://ec2-54-183-212-199.us-west-1.compute.amazonaws.com:8080'
 };
